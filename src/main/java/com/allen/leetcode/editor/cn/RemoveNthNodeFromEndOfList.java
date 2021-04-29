d9f8f9f66bbb9ee3dc965432ecb8bd8af7bb5c84
窦建新 <180449@gree.com.cn>
2021-04-29 17:12:01 +0800

    19 删除链表的倒数第那个节点

---
+++ src/main/java/com/allen/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,94 @@
+// 题目编号： 19
+//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 
+//
+// 进阶：你能尝试使用一趟扫描实现吗？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], n = 2
+//输出：[1,2,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1], n = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1,2], n = 1
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中结点的数目为 sz 
+// 1 <= sz <= 30 
+// 0 <= Node.val <= 100 
+// 1 <= n <= sz 
+// 
+// Related Topics 链表 双指针 
+// 👍 1342 👎 0
+
+
+package com.allen.leetcode.editor.cn;
+
+/**
+ * 设置两个指针，第一个指针距离前一个指针n个距离，同时移动两个指针
+ * 两个指针移动相同距离，当一个指针移动到终点时，前一个指针就是第n个节点了
+ */
+public class RemoveNthNodeFromEndOfList{
+    public static void main(String[] args) {
+         Solution solution = new RemoveNthNodeFromEndOfList().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head;
+        ListNode slow = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        if (fast == null) {
+            head = head.next;
+            return head;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+}
